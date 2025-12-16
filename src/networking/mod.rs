882a86ed2acc88aab5388c1d62ea4891d23cd404
hchap1 <hchap1@hotmail.com>
2025-12-17 01:09:08 +1000

    chore: change protocol

--- src/networking/mod.rs
+++ src/networking/mod.rs
@@ -1,21 +1,13 @@
 /*
+    Overview
+    Each 'Server' is a permanent IROH node address stored in a database. The Server listens for clients and accepts them as long as they provide the correct ALPN.
+    Each of the Server's clients is a foreign Server. All communication is done through single-use bidirectional streams.
+    Each complete packet exchance (full message / file) is verified after transmission.
+
     Protocol
-    
-    Streams (as per QUIC) are a one-time-use for a single distinct message.
-    Each instance will own a Server which exclusively receives streams from incoming connections.
-    When a node wishes to message another node, that node must spawn a client (long lived) that will transmit the message via short lived streams.
-    Servers will have persistent IROH node addresses. Client node addresses are not persistent.
-
-    Security / Identity
-    Each Server has a persistent IROH node address.
-    When a Server creates a Client to message another Server, the Client will include the node address of the Server, and an authentication code. The foreign Server will attempt to establish its own Client-Server connection with the Server, and upon success, will send the original authentication code as well as a foreign authentication code. The original Server can check the received codes, one of which should be the original, and one is a new one that it must relay back via the Client to the foreign Server. The foreign Server must listen for the code being echoed back by the client of the Server it just connected to.
-
-    This prevents a Client pretending to be associated with a Server and ensures full-circle linked communication exists.
-
-    If the foreign Server cannot connect with the original Server, either a technical failure has occured or the Client was lying about its Server.
-    If the original Server receives no new connection, then that Server does not exist or is not a part of this protocol.
-    If the original Server receives a connection request with a code it did not send, the connection request is invalid and will be refused.
-    If the foreign Server does not receive its code back via the original Client, then the original Client is lying about its Server.
+    The first byte of the packet identifies its type.
+    The second-fifth bytes of the packet are a unique 32-bit identifier for that packet that must be echoed back to confirm transmission.
+    The rest of the bytes are 'data' as defined by the standard for that packet type.
 */
 
 const ALPN: &[u8] = b"hchap1/v1";
