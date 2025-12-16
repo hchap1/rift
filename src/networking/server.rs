b0f1adc32ea77f5522091c198a59c1113af6cb16
hchap1 <hchap1@hotmail.com>
2025-12-16 23:43:17 +1000

    feat: connection manager linked to server via channels

--- src/networking/server.rs
+++ src/networking/server.rs
@@ -5,6 +5,8 @@ use iroh::endpoint::Connection;
 use crate::error::Res;
 use crate::networking::ALPN;
 use crate::networking::connection_manager::ConnectionManager;
+use crate::networking::connection_manager::ConnectionManagerMessage;
+use crate::util::channel::send;
 
 pub struct Local {
     endpoint: Endpoint,
@@ -24,8 +26,19 @@ impl Local {
             connection_manager: ConnectionManager::new(endpoint)
         })
     }
+
+    pub fn connect_task(&self, target: EndpointAddr) -> impl std::future::Future<Output = Res<()>> {
+        let endpoint = self.endpoint.clone();
+        let sender = self.connection_manager.yield_sender();
+        async move {
+            let foreign = Foreign::establish(endpoint, target).await?;
+            send(ConnectionManagerMessage::Add(foreign), &sender).await?;
+            Ok(())
+        }
+    }
 }
 
+#[derive(Clone, Debug)]
 pub struct Foreign {
     stable_id: usize,
     connection: Connection
@@ -43,5 +56,9 @@ impl Foreign {
     pub async fn establish(endpoint: Endpoint, target: EndpointAddr) -> Res<Foreign> {
         let connection = endpoint.connect(target, ALPN).await?;
         Ok(Foreign { stable_id: connection.stable_id(), connection })
+    }
+
+    pub fn stable_id(&self) -> usize {
+        self.stable_id
     }
 }
