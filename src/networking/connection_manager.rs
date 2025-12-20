b606e6d200e6bc44521be324b6e4ce022d6e43f5
hchap1 <hchap1@hotmail.com>
2025-12-20 16:27:54 +1000

    fix: distribute open-ended MPMC sender for packet collection

--- src/networking/connection_manager.rs
+++ src/networking/connection_manager.rs
@@ -1,16 +1,18 @@
 use std::collections::HashMap;
 
+use async_channel::Sender;
 use async_channel::unbounded;
 use iroh::Endpoint;
 use tokio::task::JoinHandle;
 
 use crate::error::Error;
 use crate::error::Res;
+use crate::networking::packet::Packet;
 use crate::networking::server::Foreign;
 use crate::util::channel::send;
 
-type Sender = async_channel::Sender<ConnectionManagerMessage>;
-type Receiver = async_channel::Receiver<ConnectionManagerMessage>;
+type Send = async_channel::Sender<ConnectionManagerMessage>;
+type Recv = async_channel::Receiver<ConnectionManagerMessage>;
 
 #[derive(Debug, Clone)]
 pub enum ConnectionManagerMessage {
@@ -22,25 +24,25 @@ pub enum ConnectionManagerMessage {
 pub struct ConnectionManager {
     _listen_handle: JoinHandle<Res<()>>,
     _manage_handle: JoinHandle<Res<()>>,
-    sender_to_thread: Sender,
-    output: Receiver
+    sender_to_thread: Send,
+    output: Recv
 }
 
 impl ConnectionManager {
 
-    pub fn new(endpoint: Endpoint) -> ConnectionManager {
+    pub fn new(endpoint: Endpoint, packet_sender: Sender<Packet>) -> ConnectionManager {
         let (thread_sender, thread_receiver) = unbounded();
         let (output_sender, output_receiver) = unbounded();
 
         ConnectionManager {
-            _listen_handle: tokio::task::spawn(Self::listen(endpoint, thread_sender.clone(), output_sender.clone())),
+            _listen_handle: tokio::task::spawn(Self::listen(endpoint, thread_sender.clone(), output_sender.clone(), packet_sender)),
             _manage_handle: tokio::task::spawn(Self::manage(thread_receiver, output_sender)),
             sender_to_thread: thread_sender,
             output: output_receiver
         }
     }
 
-    async fn listen(endpoint: Endpoint, task_sender: Sender, output: Sender) -> Res<()> {
+    async fn listen(endpoint: Endpoint, task_sender: Send, output: Send, packet_sender: Sender<Packet>) -> Res<()> {
         loop {
             let res = match endpoint.accept().await {
                 Some(accept) => accept.await,
@@ -52,13 +54,13 @@ impl ConnectionManager {
             match res {
 
                 // A new connection has been aquired.
-                Ok(connection) => send(ConnectionManagerMessage::Add(Foreign::new(connection)), &task_sender).await?,
+                Ok(connection) => send(ConnectionManagerMessage::Add(Foreign::new(connection, packet_sender.clone())), &task_sender).await?,
                 Err(e) => send(ConnectionManagerMessage::Error(e.into()), &output).await?
             }
         }
     }
 
-    async fn manage(receiver: Receiver, sender: Sender) -> Res<()> {
+    async fn manage(receiver: Recv, sender: Send) -> Res<()> {
 
         let mut connections: HashMap<usize, Foreign> = HashMap::new();
 
@@ -73,7 +75,7 @@ impl ConnectionManager {
         Ok(())
     }
 
-    pub fn yield_sender(&self) -> Sender {
+    pub fn yield_sender(&self) -> Send {
         self.sender_to_thread.clone()
     }
 }
--- src/networking/server.rs
+++ src/networking/server.rs
@@ -1,5 +1,8 @@
 use std::sync::Arc;
 
+use async_channel::Receiver;
+use async_channel::Sender;
+use async_channel::unbounded;
 use iroh::Endpoint;
 use iroh::EndpointAddr;
 use iroh::endpoint::Connection;
@@ -9,11 +12,14 @@ use crate::networking::ALPN;
 use crate::networking::connection_manager::ConnectionManager;
 use crate::networking::connection_manager::ConnectionManagerMessage;
 use crate::networking::foreign_manager::ForeignManager;
+use crate::networking::packet::Packet;
 use crate::util::channel::send;
 
 pub struct Local {
     endpoint: Endpoint,
-    connection_manager: ConnectionManager
+    connection_manager: ConnectionManager,
+    packet_sender: Sender<Packet>,
+    packet_receiver: Receiver<Packet>
 }
 
 impl Local {
@@ -24,17 +30,23 @@ impl Local {
             .bind()
             .await?;
 
+        let (packet_sender, packet_receiver) = unbounded();
+
         Ok(Local {
             endpoint: endpoint.clone(),
-            connection_manager: ConnectionManager::new(endpoint)
+            connection_manager: ConnectionManager::new(endpoint, packet_sender.clone()),
+            packet_sender,
+            packet_receiver
         })
     }
 
     pub fn connect_task(&self, target: EndpointAddr) -> impl std::future::Future<Output = Res<()>> {
         let endpoint = self.endpoint.clone();
         let sender = self.connection_manager.yield_sender();
+        let packet_sender = self.packet_sender.clone();
+
         async move {
-            let foreign = Foreign::establish(endpoint, target).await?;
+            let foreign = Foreign::establish(endpoint, target, packet_sender).await?;
             send(ConnectionManagerMessage::Add(foreign), &sender).await?;
             Ok(())
         }
@@ -49,16 +61,16 @@ pub struct Foreign {
 
 impl Foreign {
 
-    pub fn new(connection: Connection) -> Foreign {
+    pub fn new(connection: Connection, packet_sender: Sender<Packet>) -> Foreign {
         Foreign {
             stable_id: connection.stable_id(),
-            foreign_manager: Arc::new(ForeignManager::new(connection))
+            foreign_manager: Arc::new(ForeignManager::new(connection, packet_sender))
         }
     }
     
-    pub async fn establish(endpoint: Endpoint, target: EndpointAddr) -> Res<Foreign> {
+    pub async fn establish(endpoint: Endpoint, target: EndpointAddr, packet_sender: Sender<Packet>) -> Res<Foreign> {
         let connection = endpoint.connect(target, ALPN).await?;
-        Ok(Foreign::new(connection))
+        Ok(Foreign::new(connection, packet_sender))
     }
 
     pub fn stable_id(&self) -> usize {
