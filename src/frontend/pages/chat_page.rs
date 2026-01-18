a7287ee269fee2de7a0f678f1a4546d5c0eb9351
hchap1 <hchap1@hotmail.com>
2026-01-19 00:04:39 +1000

    fix: syntax error

--- src/frontend/pages/chat_page.rs
+++ src/frontend/pages/chat_page.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, mem::take, path::PathBuf};
 use iced::{Background, Border, Length, Shadow, Task, widget::{Column, Container, Row, Scrollable, button, scrollable::{AutoScroll, Rail, Scroller}, text, text_input}};
 
-use crate::{backend::chat::Chat, error::{Error, Res}, frontend::{application::Ph age, message::{Global, Message}, widget::Colour}, networking::packet::{Packet, TrackedPacket, TrackedPacketResponse}};
+use crate::{backend::chat::Chat, error::{Error, Res}, frontend::{application::Page, message::{Global, Message}, widget::Colour}, networking::packet::{Packet, TrackedPacket, TrackedPacketResponse}};
 
 #[derive(Debug, Clone)]
 pub enum ChatMessage {
